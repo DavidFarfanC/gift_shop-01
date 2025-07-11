bccd376a454903d463d6853b5f53d3c96691d2b7
DavidFarfanC <farfan.castro.david@gmail.com>
2025-07-11 09:55:37 -0600

    Agrega .gitignore y archivos del proyecto

---
+++ run_app.bat
@@ -0,0 +1,4 @@
+@echo off
+title Gift Shop Management System
+"C:\Users\Tienda de Regalos\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.12_qbz5n2kfra8p0\python.exe" main.py
+pause
